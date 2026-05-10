37fc0ea738c895a4665018b9270528195cdc8e1a
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-05-10 22:04:04 +0600

    again

--- script/index.js
+++ script/index.js
@@ -1,7 +1,22 @@
-let loadInformation =async()=>{
-    let res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=germany&appid=13e1d3e35d715a066c58da70d0d66290&units=metric')
+let loadInformation =async(isSearch='berlin')=>{
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${isSearch}&appid=13e1d3e35d715a066c58da70d0d66290&units=metric`)
     let data = await res.json()
-    console.log(data);
+    let results = data.main
+    let name = data.name
+    console.log(name)
+    disPlayShow(results)
+}
+let disPlayShow = (results)=>{
+    console.log(results)
+    let temp = document.getElementById('temp')
+    let humidity = document.getElementById('humidity')
+    let wind = document.getElementById('wind')
+    for(let key in results){
+        console.log(key, results[key])
+        temp.innerText = results.temp
+        humidity.innerText = results.humidity
+       
+    }
 }
 
 // access all 
@@ -12,9 +27,8 @@ let searchField = document.getElementById('searchField')
 
 let searchBtn = ()=>{
     let searchText = searchField.value;
-    console.log(searchText)
+    loadInformation(searchText);
 }
 
 
-
-loadInformation();
+loadInformation()
