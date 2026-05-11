297eeb5cf47df2996c69ba6269060dc13a4df707
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-05-11 16:35:36 +0600

    done

--- script/index.js
+++ script/index.js
@@ -1,21 +1,39 @@
-let loadInformation =async(isSearch='berlin')=>{
+let loadInformation =async(isSearch)=>{
     let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${isSearch}&appid=13e1d3e35d715a066c58da70d0d66290&units=metric`)
     let data = await res.json()
     let results = data.main
-    let name = data.name
-    console.log(name)
-    disPlayShow(results)
+    let name = data.name;
+    let wind = data.wind.speed
+    let weather = data.weather[0].main
+    // console.log(weather)
+    // console.log(wind)
+    // console.log(name)
+    // console.log(data)
+    disPlayShow(results,name,wind,weather)
 }
-let disPlayShow = (results)=>{
-    console.log(results)
+let disPlayShow = (results,name,wind,weather)=>{
+    // console.log(results)
+    // console.log(wind)
+    // console.log(name)
+    let weatherContainer = document.getElementById('weatherContainer')
+    weatherContainer.textContent = '';
+    let weatherDetails = document.createElement('div')
+    weatherDetails.innerHTML = `
+        <p>${weather}</p>
+    `
+    weatherContainer.appendChild(weatherDetails)
+
+
+    let cityName = document.getElementById('cityName')
     let temp = document.getElementById('temp')
     let humidity = document.getElementById('humidity')
-    let wind = document.getElementById('wind')
+    let winds = document.getElementById('wind')
     for(let key in results){
         console.log(key, results[key])
-        temp.innerText = results.temp
-        humidity.innerText = results.humidity
-       
+        temp.innerText = results.temp + "°C"
+        humidity.innerText = results.humidity + "%"
+       cityName.innerText = name;
+       winds.innerText = wind;
     }
 }
 
@@ -31,4 +49,4 @@ let searchBtn = ()=>{
 }
 
 
-loadInformation()
+// loadInformation()
